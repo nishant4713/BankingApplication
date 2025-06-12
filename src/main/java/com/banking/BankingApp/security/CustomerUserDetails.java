9e75ac6870f5510f8852a37459ea103425337aaf
nishant4713 <108873242+nishant4713@users.noreply.github.com>
2025-06-12 16:45:34 +0530

    setup JWT-based authentication and security config

---
+++ src/main/java/com/banking/BankingApp/security/CustomerUserDetails.java
@@ -0,0 +1,18 @@
+package com.banking.BankingApp.security;
+
+import com.banking.BankingApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomerUserDetails implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return user;
+    }
+}
